cce6f8a115852ee86c65713eac7c130cc83e9d60
Admin <Admin@DESKTOP-89NTJEN>
2019-05-22 13:23:25 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simplecalculator
+{
+    class Program
+    {
+        //program that takes two numbers as input and perform an operation (+,-,*,x,/) on them and displays the result of that operation.
+        static void Main(string[] args)
+        {
+            int Num1, Num2, result;
+            char option;
+            Console.Write("Enter the First Number : ");
+            Num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Operation you want to perform : ");
+            option = char.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Second Number : ");
+            Num2 = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case '+':
+                    result = Num1 + Num2;
+                    Console.WriteLine("The result of Addition is : {0}", result);
+                    break;
+                case '-':
+                    result = Num1 - Num2;
+                    Console.WriteLine("The result of Subtraction is : {0}", result);
+                    break;
+                case '*':
+                    result = Num1 * Num2;
+                    Console.WriteLine("The result of Multiplication is : {0}", result);
+                    break;
+                case '/':
+                    result = Num1 / Num2;
+                    Console.WriteLine("The result of Division is : {0}", result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        }
+    }
